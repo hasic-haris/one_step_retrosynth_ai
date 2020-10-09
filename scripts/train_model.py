a6cf06c3d57cde3b0ec9990e103834d135821fa0
hasic-haris-research <hasic-haris@hotmail.com>
2020-10-09 12:40:23 +0900

    Implementing cross-validation monitoring.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -10,8 +10,8 @@ from model_methods.model_construction import train_model, test_model
 
 full_config = FullConfig.load()
 
-print("\nStep 1/2: Train the models with the specified hyper-parameters.\n")
-train_model(full_config, specific_folds=[1, 2])
+#print("\nStep 1/2: Train the models with the specified hyper-parameters.\n")
+#train_model(full_config, specific_folds=[1, 2])
 
-print("\nStep 1/2: Test the trained models.\n")
+print("\nStep 2/2: Test the trained models.\n")
 test_model(full_config, specific_folds=[1, 2])
