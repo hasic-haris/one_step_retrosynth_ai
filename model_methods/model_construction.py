8d5f89e4487aadef919b37b64253353d3706c94b
hasic-haris-research <hasic-haris@hotmail.com>
2020-10-10 10:37:05 +0900

    Implementing final evaluation scripts.

--- model_methods/model_construction.py
+++ model_methods/model_construction.py
@@ -436,7 +436,8 @@ def apply_model(args, input_data=None):
     log_folder = generate_log_folder(model_config["logs_folder"], args.evaluation_config.best_fold,
                                      args.evaluation_config.best_input_config["folder_name"])
 
-    input_data = pd.read_pickle(args.evaluation_config.final_data_model) if input_data is None else input_data
+    input_data = pd.read_pickle(args.evaluation_config.final_data_model).to_numpy() if input_data is None \
+        else input_data
 
     # Create the instance of the TensorFlow graph.
     tf_model_graph = tf.Graph()
